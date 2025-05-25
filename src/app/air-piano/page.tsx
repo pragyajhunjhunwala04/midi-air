3412fa3910089f701ee942a3ee3f0f59d730e3b4
Carson Davis <cmd52465@gmail.com>
2025-05-24 22:25:15 -0700

    refactor: camera component

--- src/app/air-piano/page.tsx
+++ src/app/air-piano/page.tsx
@@ -1,102 +1,26 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
-import {
-	FilesetResolver,
-	GestureRecognizer,
-	GestureRecognizerResult,
-} from "@mediapipe/tasks-vision";
+import React, { useRef, useState } from "react";
 
-import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import { Piano, MidiNumbers } from "react-piano";
 import "react-piano/dist/styles.css";
 import { getNoteFromGestures, playNote } from "@/utils/audio";
-import { SimplifiedGestures, simplifyGestures } from "@/utils/gestures";
+import { SimplifiedGestures } from "@/utils/gestures";
+import Camera from "@/components/Camera";
 
 export default function AirPiano() {
-	const videoRef = useRef<HTMLVideoElement>(null);
-	const [gestureRecognizer, setGestureRecognizer] =
-		useState<GestureRecognizer | null>(null);
-	const [gestureResult, setGestureResult] =
-		useState<SimplifiedGestures | null>(null);
+	const [gestures, setGestures] = useState<SimplifiedGestures | null>(null);
 	const previousNoteRef = useRef<string>("");
 	const noteBufferRef = useRef<string[]>([]);
-	const STABILITY_THRESHOLD = 3; // how many consistent frames needed
 
+	const STABILITY_THRESHOLD = 3; // how many consistent frames needed
 	const firstNote = MidiNumbers.fromNote("c1"); // MIDI number for C3
 	const lastNote = MidiNumbers.fromNote("c5"); // MIDI number for F5
 
-	useEffect(() => {
-		let animationFrameId: number;
-		let lastVideoTime = -1;
-		let running = true;
-
-		async function setupGestureRecognizer() {
-			// Create task for image file processing:
-			const vision = await FilesetResolver.forVisionTasks(
-				// path/to/wasm/root
-				"https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-			);
-			const recognizer = await GestureRecognizer.createFromOptions(
-				vision,
-				{
-					baseOptions: {
-						modelAssetPath:
-							// "https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/gesture_recognizer.task",`
-							"./asl_gesture_recognizer.task",
-					},
-				}
-			);
-
-			await recognizer.setOptions({ runningMode: "VIDEO", numHands: 2 });
-
-			setGestureRecognizer(recognizer);
-
-			// Start webcam
-			if (videoRef.current) {
-				const stream = await navigator.mediaDevices.getUserMedia({
-					video: true,
-				});
-				videoRef.current.srcObject = stream;
-				await videoRef.current.play();
-			}
-
-			// Render loop for gesture detection
-			function renderLoop() {
-				if (!videoRef.current || !recognizer || !running) return;
-				if (videoRef.current.currentTime !== lastVideoTime) {
-					const result = recognizer.recognizeForVideo(
-						videoRef.current,
-						videoRef.current.currentTime
-					);
-					// console.log(result);
-					const simplified = simplifyGestures(result);
-					setGestureResult(simplified);
-					lastVideoTime = videoRef.current.currentTime;
-				}
-				animationFrameId = requestAnimationFrame(renderLoop);
-			}
-
-			renderLoop();
-		}
-
-		setupGestureRecognizer();
-
-		return () => {
-			running = false;
-			if (gestureRecognizer) {
-				gestureRecognizer.close();
-			}
-			if (videoRef.current?.srcObject) {
-				(videoRef.current.srcObject as MediaStream)
-					.getTracks()
-					.forEach((track) => track.stop());
-			}
-			cancelAnimationFrame(animationFrameId);
-		};
-	}, []);
-
-	useEffect(() => {
+	const handleGestures = (gestureResult: SimplifiedGestures | null) => {
 		if (!gestureResult) return;
+
+		setGestures(gestureResult);
 
 		const currentNote = getNoteFromGestures(gestureResult);
 		const buffer = noteBufferRef.current;
@@ -125,31 +49,25 @@ export default function AirPiano() {
 			playNote(buffer[0]);
 			previousNoteRef.current = buffer[0];
 		}
-	}, [gestureResult]);
+	};
 
 	return (
 		<div className='flex flex-col items-center m-auto'>
 			<div className='flex flex-3'>
 				<div className='h-[480px] w-[200px] border border-white/20 bg-white bg-opacity-10 py-8 px-4 rounded-xl mx-4 text-md text-opacity-75'></div>
-				<video
-					id='video'
-					ref={videoRef}
-					className='w-[640px] h-[480px] rounded-lg border border-gray-300 scale-x-[-1]'
-					muted
-					playsInline
-				/>
+				<Camera onGesturesDetected={handleGestures} />
 				<div className='h-[480px] w-[200px] border border-white/20 bg-white bg-opacity-10 py-8 px-4 rounded-xl mx-4 text-md text-opacity-75'>
 					<h2 className='text-lg font-semibold mb-2'>
 						Detected Gestures:
 					</h2>
-					{gestureResult ? (
+					{gestures ? (
 						<>
 							<div className='mb-3'>
 								<div className='font-semibold mb-1'>
 									Left Hand:
 								</div>
 								<div className='mb-1 ml-2'>
-									<strong>{gestureResult["left"]}</strong>
+									<strong>{gestures["left"]}</strong>
 								</div>
 							</div>
 							<div className='mb-3'>
@@ -157,7 +75,7 @@ export default function AirPiano() {
 									Right Hand:
 								</div>
 								<div className='mb-1 ml-2'>
-									<strong>{gestureResult["right"]}</strong>
+									<strong>{gestures["right"]}</strong>
 								</div>
 							</div>
 						</>
